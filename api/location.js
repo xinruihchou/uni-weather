d84434788e9fdd8956bbfe4e5357439452cba183
kakala1024 <kakala1024@devide>
2023-10-10 20:31:00 +0800

    Tue Oct 10 20:31:00 CST 2023 inscode

--- api/location.js
+++ api/location.js
@@ -1,20 +1,25 @@
+// 引入 request.js 模块
 import {
 	request
-} from './request.js'
-export function getGeodecode(longitude, latitude) {
+  } from './request.js'
+  
+  // 定义 getGeodecode 方法，接受经度和纬度两个参数
+  export function getGeodecode(longitude, latitude) {
+	// 返回一个 Promise 对象，回调函数接受 resolve 和 reject 参数
 	return new Promise((resolve, reject) => {
-		request({
-			url: '/location/geodecode',
-			data: {
-				longitude,
-				latitude,
-				APPKEY:'NSSFSUUR3R9T'
-			}
-		}).then((data) => {
-			resolve(data)
-		}).catch((e) => {
-			reject(e)
-		})
+	  // 请求服务器接口，获取地理位置信息
+	  request({
+		url: '/location/geodecode', // 请求地址
+		data: { // 请求参数
+		  longitude, // 经度
+		  latitude, // 纬度
+		  APPKEY: 'NSSFSUUR3R9T' // APPKEY，用于鉴权
+		}
+	  }).then((data) => { // 请求成功回调函数
+		resolve(data) // 将获取到的地理位置信息传入 resolve 函数
+	  }).catch((e) => { // 请求失败回调函数
+		reject(e) // 将错误信息传入 reject 函数
+	  })
 	})
-
-}
+  }
+  
--- api/request.js
+++ api/request.js
@@ -1,19 +1,29 @@
+// 定义常量 PRE_HTTP，表示接口的前缀
 const PRE_HTTP = "https://api.gugudata.com"
+
+// 定义request函数，接收一个对象作为参数，包括url、method、data三个属性
 export function request({
 	url,
 	method = 'get',
 	data
 }) {
 	return new Promise((resolve, reject) => {
+		// 调用 uni.request 发送请求
 		uni.request({
+			// 拼接完整的请求地址
 			url: PRE_HTTP + url,
+			// 定义请求方法，默认为 GET
 			method,
+			// 定义请求参数
 			data,
+			// 请求成功回调函数
 			success(data) {
 				resolve(data)
-			},fail(e){
+			},
+			// 请求失败回调函数
+			fail(e){
 				reject(e)
 			}
 		})
 	})
-}
+} 
